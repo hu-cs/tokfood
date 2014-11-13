2b3f4fe7e959fa3a0dc81fca53e4a96b21ae70f5
tokhi <shafi.tokhi@ilovevideo.tv>
2014-11-13 16:44:13 +0100

    sending reciepies to all users via mandril

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -15,24 +15,33 @@ class AdminMailer < ActionMailer::Base
   	AdminMailer.new_user(self).deliver  	
   end
 
-  def weekly_recipes(recipes)
+  def weekly_recipes(recipes, users)
 
   	template_name = 'new-reciepe'
   	template_content = []
-  	message = {
-  		to: [{email: "shafi.tokhi@gmail.com"}],
-  		subject: "test subject",
-  		merge_vars: [
-  			{rcpt: "shafi.tokhi@gmail.com",
-  				vars: [
-  					{name: "", content: ""}
-  				]
+    users.each do | u |
+    	message = {
+    		to: [{email: u.email}],
+    		subject: "Popular recipes by ",
+    		merge_vars: [
+    			{rcpt: u.email,
+    				vars: [
+    					{name: "IMAGE1", content: recipes[0].image},
+              {name: "TITLE1", content: recipes[0].title},
+              {name: "IMAGE2", content: recipes[1].image},
+              {name: "TITLE2", content: recipes[1].title},
+              {name: "IMAGE3", content: recipes[2].image},
+              {name: "TITLE3", content: recipes[2].title},
+              {name: "IMAGE4", content: recipes[3].image},
+              {name: "TITLE4", content: recipes[3].title}
+    				]
 
-  			}
+    			}
 
-  		]
-  	}
-  	mandrill_client.messages.send_template template_name, template_content, message
+    		]
+    	}
+    	mandrill_client.messages.send_template template_name, template_content, message
+    end
   	
   end
 end
--- config/initializers/schedualer.rb
+++ config/initializers/schedualer.rb
@@ -4,72 +4,78 @@ require 'open-uri'
 
 scheduler = Rufus::Scheduler.new
 
-scheduler.every '20h' do
-  puts "do something in every 15 seconds.."
-  scrap_foodily
+scheduler.every '1m' do
+  puts "do something in every days.."
+  send_weekly_emails_to_users
 end
 
-def scrap_foodily
-	site = 'http://www.foodily.com'
-	keys = ["vegetarian","egg","easy","pizza","soup","german","european","seafood","spaghetti","grill","steak", "kebab","milk","yummy", "irani", "eastern", "western" , "delicious","chicken", "breakfast", "dinner", "lunch", "sheep", "fish", "potato","tomato", "oil", "cream", "ice", "drink", "sweet", "fat", "burger","asian","persian", "arabic"]
-	keys.each do | key |
-		foodily = Nokogiri::HTML(open(site+"/s/#{key}"))
-		recipes = foodily.xpath('//*[@id="cards_inner"]')[0].children
+def send_weekly_emails_to_users
+  recipes = Recipe.last(6)
+  users = User.all
+  AdminMailer.weekly_recipes(recipes,users)
+end
 
-		recipes.each do | recipe |
-			#img = recipe.children[1].children[1].children[1].attributes["src"].text.strip
-			begin
-			recipe_url = site + recipe.attributes["link"].text
+# def scrap_foodily
+# 	site = 'http://www.foodily.com'
+# 	keys = ["vegetarian","egg","easy","pizza","soup","german","european","seafood","spaghetti","grill","steak", "kebab","milk","yummy", "irani", "eastern", "western" , "delicious","chicken", "breakfast", "dinner", "lunch", "sheep", "fish", "potato","tomato", "oil", "cream", "ice", "drink", "sweet", "fat", "burger","asian","persian", "arabic"]
+# 	keys.each do | key |
+# 		foodily = Nokogiri::HTML(open(site+"/s/#{key}"))
+# 		recipes = foodily.xpath('//*[@id="cards_inner"]')[0].children
 
-			rescue
-				next
-			end
+# 		recipes.each do | recipe |
+# 			#img = recipe.children[1].children[1].children[1].attributes["src"].text.strip
+# 			begin
+# 			recipe_url = site + recipe.attributes["link"].text
+
+# 			rescue
+# 				next
+# 			end
 			
-			m_recipe = Nokogiri::HTML(open(recipe_url))
+# 			m_recipe = Nokogiri::HTML(open(recipe_url))
 
-			origional_link = m_recipe.xpath('//*[@id="recipe"]/div[8]/div[4]/div/div/a/@href').text
+# 			origional_link = m_recipe.xpath('//*[@id="recipe"]/div[8]/div[4]/div/div/a/@href').text
 
-			r = Recipe.where("url = '#{origional_link}'")
-			if(!r.empty?)
-				puts "already stored!!"
-				next
-			end
+# 			r = Recipe.where("url = '#{origional_link}'")
+# 			if(!r.empty?)
+# 				puts "already stored!!"
+# 				next
+# 			end
 
-			title = m_recipe.xpath('//*[@id="recipe"]/div[8]/div[1]/div/div/h1').text
-			img = m_recipe.xpath('//*[@id="recipe"]/div[8]/div[2]/div/div/div[2]/img/@src').text
-			ing_size = m_recipe.xpath('//*[@id="recipe"]/div[8]/div[3]/div/div/div/div/p').count
-			ingredients = ""
+# 			title = m_recipe.xpath('//*[@id="recipe"]/div[8]/div[1]/div/div/h1').text
+# 			img = m_recipe.xpath('//*[@id="recipe"]/div[8]/div[2]/div/div/div[2]/img/@src').text
+# 			ing_size = m_recipe.xpath('//*[@id="recipe"]/div[8]/div[3]/div/div/div/div/p').count
+# 			ingredients = ""
 
-			puts "count value: #{ing_size}"
+# 			puts "count value: #{ing_size}"
 
-			i = 0
-			while i < ing_size  do
-			   ingredients += m_recipe.xpath('//*[@id="recipe"]/div[8]/div[3]/div/div/div/div/p')[i].text.strip+","
-			   i +=1
-			end
+# 			i = 0
+# 			while i < ing_size  do
+# 			   ingredients += m_recipe.xpath('//*[@id="recipe"]/div[8]/div[3]/div/div/div/div/p')[i].text.strip+","
+# 			   i +=1
+# 			end
 
 			
 
 
-			tags_xpath = m_recipe.xpath('//*[@id="recipe"]/div[8]/div[1]/div/div/div[3]/a').children
-			tags = ""
-			tags_xpath.each do |t|
-				tags+= "#{t},"
-			end
-			puts "~> #{title}\n"
-			# puts "url:\n#{recipe_url}\n"
-			# puts "img:\n#{img}\n"
-			# puts "origional_link:\n#{origional_link}\n"
-			# puts "ingredients:\n#{ingredients}"
-			# puts "tags:\n#{tags}"
-			# puts "\n\n"
-			# save recipe to db
-			save_recipe(title, img, ingredients, origional_link, tags)
+# 			tags_xpath = m_recipe.xpath('//*[@id="recipe"]/div[8]/div[1]/div/div/div[3]/a').children
+# 			tags = ""
+# 			tags_xpath.each do |t|
+# 				tags+= "#{t},"
+# 			end
+# 			puts "~> #{title}\n"
+# 			# puts "url:\n#{recipe_url}\n"
+# 			# puts "img:\n#{img}\n"
+# 			# puts "origional_link:\n#{origional_link}\n"
+# 			# puts "ingredients:\n#{ingredients}"
+# 			# puts "tags:\n#{tags}"
+# 			# puts "\n\n"
+# 			# save recipe to db
+# 			save_recipe(title, img, ingredients, origional_link, tags)
 
-		end
-	end
+# 		end
+# 	end
 	
-end
+# end
 
 def save_recipe(title, img, ingredients, origional_link, tags)
 	# r = Recipe.where("url = '#{origional_link}'")
