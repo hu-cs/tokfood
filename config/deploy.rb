6d68120be2f7faa0cd1937185d4d9f7a04c708e7
tokhi <shafi.tokhi@gmail.com>
2015-03-01 19:33:55 +0100

    update deploy file

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :repo_url, 'git@github.com:hu-cs/tokfood.git'
 
 # Default value for :scm is :git
 #set :scm, :git
-set :branch, 'master'
+set :branch, 'emailtemplate'
 
 # Default value for :format is :pretty
 # set :format, :pretty
